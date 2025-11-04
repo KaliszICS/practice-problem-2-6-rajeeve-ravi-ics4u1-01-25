e55ad3a34eb221681dbeaab5eb650294c2e4c52c
rajeeve-ravi-ics4u1-01-25 <rajeeve.ravi@student.tdsb.on.ca>
2025-11-04 15:10:29 -0500

    Stu submit V1

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,309 +1,138 @@
-import java.lang.reflect.Method;
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.*;
-import java.io.*;
-public class PracticeProblemTest {
-@Test
-@DisplayName("")
-void insertionSortTest1() {
-    Class<?> testClass = PracticeProblem.class;
-    try {
-        Class<?>[] cArg = {double[].class};
-        Method method = testClass.getDeclaredMethod("insertionSort", cArg);
-  // Enter code here
-  double arr[] = {155.2, 155.1, 1, 5, 3, 2, 7};
-  double arr2[] = {1, 2, 3, 5, 7, 155.1, 155.2};
-    method.invoke(null, arr);
-  assertArrayEquals(arr2, arr, 0.1);
-}
+public class PracticeProblem {
+
+	public static void main(String args[]) {
+
+	}
+    public static int[] bubbleSort(double[] nums) {
+        int swaps = 0;
+        int steps = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                steps++; // Comparison
+                if (nums[j] > nums[j + 1]) {
+                    // Swap
+                    double temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                    swaps += 3; // Each swap counts as 3
+                    swapped = true;
+                }
+            }
+            if (!swapped) break; // Optimization
+        }
+
+        return new int[]{swaps, steps};
+    }
 
 
-    catch (NoSuchMethodException e) {
-        fail("Method does not exist");
-}
-    catch(Exception e) {
-        fail("Something weird happened");
-}
-}
-@Test
-@DisplayName("")
-void insertionSortTest2() {
-    Class<?> testClass = PracticeProblem.class;
-    try {
-        Class<?>[] cArg = {double[].class};
-        Method method = testClass.getDeclaredMethod("insertionSort", cArg);
-  // Enter code here
-  double arr[] = {155.2, 155.1, 1, 5, 3, 2, 7};
-  double arr2[] = {1, 2, 3, 5, 7, 155.1, 155.2};
-    int[] result = (int[])method.invoke(null, arr);
-  int[] expected = new int[]{26,14};
-  assertArrayEquals(expected, result);
-}
+    public static int[] selectionSort(double[] arr) {
+        int swapEvent = 0;
+        int stepEvent = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                stepEvent++;
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                    swapEvent++;
+                }
+            }
+            swapEvent += 3;
+            double temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+        return new int[]{swapEvent, stepEvent};
+    }
 
 
-    catch (NoSuchMethodException e) {
-        fail("Method does not exist");
-}
-    catch(Exception e) {
-        fail("Something weird happened");
-}
-}
-@Test
-@DisplayName("")
-void selectionSortTest1() {
-    Class<?> testClass = PracticeProblem.class;
-    try {
-        Class<?>[] cArg = {double[].class};
-        Method method = testClass.getDeclaredMethod("selectionSort", cArg);
-  // Enter code here
-  double arr[] = {155.2, 155.1, 1, 5, 3, 2, 7, 8};
-  double arr2[] = {1, 2, 3, 5, 7, 8, 155.1, 155.2};
-    method.invoke(null, arr);
-  assertArrayEquals(arr2, arr, 0.1);
-}
+    public static int[] insertionSort(double[] nums) {
+        int swaps = 0;
+        int steps = 0;
+        int n = nums.length;
+
+        for (int i = 1; i < n; i++) {
+            double key = nums[i];
+            swaps++; // Assignment to key counts as 1
+            int j = i - 1;
+
+            while (j >= 0 && nums[j] > key) {
+                steps++; // Comparison that is true
+                nums[j + 1] = nums[j];
+                swaps++; // Single variable change counts as 1
+                j = j - 1;
+            }
+
+//            // Count the final comparison that breaks the loop
+//            if (j >= 0) {
+//                steps++;
+//            }
+
+            nums[j + 1] = key;
+            swaps++; // Final assignment counts as 1
+        }
+
+        return new int[]{swaps, steps};
+    }
+
+    public static String leastSwaps(double[] nums) {
+        // Copy arrays for each sort
+        double[] bubbleArr = nums.clone();
+        double[] selectionArr = nums.clone();
+        double[] insertionArr = nums.clone();
+
+        // Run each sort
+        int[] bubbleResult = bubbleSort(bubbleArr);
+        int[] selectionResult = selectionSort(selectionArr);
+        int[] insertionResult = insertionSort(insertionArr);
+
+        int bubbleSwaps = bubbleResult[0];
+        int selectionSwaps = selectionResult[0];
+        int insertionSwaps = insertionResult[0];
+
+        // Find minimum swaps
+        int minSwaps = Math.min(bubbleSwaps, Math.min(selectionSwaps, insertionSwaps));
+
+        // Return first alphabetically if tie
+        if (bubbleSwaps == minSwaps) {
+            return "Bubble";
+        } else if (insertionSwaps == minSwaps) {
+            return "Insertion";
+        } else {
+            return "Selection";
+        }
+    }
 
 
-    catch (NoSuchMethodException e) {
-        fail("Method does not exist");
-}
-    catch(Exception e) {
-        fail("Something weird happened");
-}
-}
-@Test
-@DisplayName("")
-void selectionSortTest2() {
-    Class<?> testClass = PracticeProblem.class;
-    try {
-        Class<?>[] cArg = {double[].class};
-        Method method = testClass.getDeclaredMethod("selectionSort", cArg);
-  // Enter code here
-  double arr[] = {155.2, 155.1, 1, 5, 3, 2, 7, 8};
-  double arr2[] = {1, 2, 3, 5, 7, 8, 155.1, 155.2};
-    int[] result = (int[])method.invoke(null, arr);
-  int[] expected = new int[]{32, 28};
-  assertArrayEquals(expected, result);
-}
+    public static String leastIterations(double[] nums) {
+        // Copy arrays for each sort
+        double[] bubbleArr = nums.clone();
+        double[] selectionArr = nums.clone();
+        double[] insertionArr = nums.clone();
 
+        // Run each sort
+        int[] bubbleResult = bubbleSort(bubbleArr);
+        int[] selectionResult = selectionSort(selectionArr);
+        int[] insertionResult = insertionSort(insertionArr);
 
-    catch (NoSuchMethodException e) {
-        fail("Method does not exist");
-}
-    catch(Exception e) {
-        fail("Something weird happened");
-}
-}
-@Test
-@DisplayName("")
-void bubbleSortTest2() {
-    Class<?> testClass = PracticeProblem.class;
-    try {
-        Class<?>[] cArg = {double[].class};
-        Method method = testClass.getDeclaredMethod("bubbleSort", cArg);
-  // Enter code here
-  double arr[] = {155.2, 155.1, 1, 5, 3, 2, 7, 235};
-  double arr2[] = {1, 2, 3, 5, 7, 155.1, 155.2, 235};
-    int[] result = (int[])method.invoke(null, arr);
-  int[] expected = new int[]{42,25};
-  assertArrayEquals(expected, result);
-}
+        int bubbleSteps = bubbleResult[1];
+        int selectionSteps = selectionResult[1];
+        int insertionSteps = insertionResult[1];
 
+        // Find minimum steps
+        int minSteps = Math.min(bubbleSteps, Math.min(selectionSteps, insertionSteps));
 
-    catch (NoSuchMethodException e) {
-        fail("Method does not exist");
-}
-    catch(Exception e) {
-        fail("Something weird happened");
-}
-}
-@Test
-@DisplayName("")
-void leastSwapsTest1() {
-    Class<?> testClass = PracticeProblem.class;
-    try {
-        Class<?>[] cArg = {double[].class};
-        Method method = testClass.getDeclaredMethod("leastSwaps", cArg);
-  // Enter code here
-  double arr[] = {155.2, 155.1, 1, 5, 3, 2, 7};
-    String result = (String)method.invoke(null, arr);
-  assertEquals("Insertion", result);
-}
+        // Return first alphabetically if tie
+        if (bubbleSteps == minSteps) {
+            return "Bubble";
+        } else if (insertionSteps == minSteps) {
+            return "Insertion";
+        } else {
+            return "Selection";
+        }
+    }
 
-
-    catch (NoSuchMethodException e) {
-        fail("Method does not exist");
-}
-    catch(Exception e) {
-        fail("Something weird happened");
-}
-}
-@Test
-@DisplayName("")
-void leastSwapsTest2() {
-    Class<?> testClass = PracticeProblem.class;
-    try {
-        Class<?>[] cArg = {double[].class};
-        Method method = testClass.getDeclaredMethod("leastSwaps", cArg);
-  // Enter code here
-  double arr[] = {1, 2, 3, 4, 5, 6};
-    String result = (String)method.invoke(null, arr);
-  assertEquals("Bubble", result);
-}
-
-
-    catch (NoSuchMethodException e) {
-        fail("Method does not exist");
-}
-    catch(Exception e) {
-        fail("Something weird happened");
-}
-}
-@Test
-@DisplayName("")
-void leastSwapsTest3() {
-    Class<?> testClass = PracticeProblem.class;
-    try {
-        Class<?>[] cArg = {double[].class};
-        Method method = testClass.getDeclaredMethod("leastSwaps", cArg);
-  // Enter code here
-  double arr[] = {155.2, 155.1, 1, 5, 3, 2, 7};
-    String result = (String)method.invoke(null, arr);
-  assertEquals("Insertion", result);
-}
-
-
-    catch (NoSuchMethodException e) {
-        fail("Method does not exist");
-}
-    catch(Exception e) {
-        fail("Something weird happened");
-}
-}
-@Test
-@DisplayName("")
-void leastSwapsTest4() {
-    Class<?> testClass = PracticeProblem.class;
-    try {
-        Class<?>[] cArg = {double[].class};
-        Method method = testClass.getDeclaredMethod("leastSwaps", cArg);
-  // Enter code here
-  double arr[] = {9, 7, 4, 2, 1, 0};
-    String result = (String)method.invoke(null, arr);
-  assertEquals("Selection", result);
-}
-
-
-    catch (NoSuchMethodException e) {
-        fail("Method does not exist");
-}
-    catch(Exception e) {
-        fail("Something weird happened");
-}
-}
-@Test
-@DisplayName("")
-void leastStepsTest1() {
-    Class<?> testClass = PracticeProblem.class;
-    try {
-        Class<?>[] cArg = {double[].class};
-        Method method = testClass.getDeclaredMethod("leastIterations", cArg);
-  // Enter code here
-  double arr[] = {155.2, 155.1, 1, 5, 3, 2, 7};
-    String result = (String)method.invoke(null, arr);
-  assertEquals("Insertion", result);
-}
-
-
-    catch (NoSuchMethodException e) {
-        fail("Method does not exist");
-}
-    catch(Exception e) {
-        fail("Something weird happened");
-}
-}
-@Test
-@DisplayName("")
-void leastStepsTest2() {
-    Class<?> testClass = PracticeProblem.class;
-    try {
-        Class<?>[] cArg = {double[].class};
-        Method method = testClass.getDeclaredMethod("leastIterations", cArg);
-  // Enter code here
-  double arr[] = {1, 2, 3, 4, 5, 6};
-    String result = (String)method.invoke(null, arr);
-  assertEquals("Insertion", result);
-}
-
-
-    catch (NoSuchMethodException e) {
-        fail("Method does not exist");
-}
-    catch(Exception e) {
-        fail("Something weird happened");
-}
-}
-@Test
-@DisplayName("")
-void leastStepsTest3() {
-    Class<?> testClass = PracticeProblem.class;
-    try {
-        Class<?>[] cArg = {double[].class};
-        Method method = testClass.getDeclaredMethod("leastIterations", cArg);
-  // Enter code here
-  double arr[] = {155.2, 155.1, 1, 5, 3, 2, 7};
-    String result = (String)method.invoke(null, arr);
-  assertEquals("Insertion", result);
-}
-
-
-    catch (NoSuchMethodException e) {
-        fail("Method does not exist");
-}
-    catch(Exception e) {
-        fail("Something weird happened");
-}
-}
-@Test
-@DisplayName("")
-void leastStepsTest4() {
-    Class<?> testClass = PracticeProblem.class;
-    try {
-        Class<?>[] cArg = {double[].class};
-        Method method = testClass.getDeclaredMethod("leastIterations", cArg);
-  // Enter code here
-  double arr[] = {9, 7, 4, 2, 1, 0};
-    String result = (String)method.invoke(null, arr);
-  assertEquals("Bubble", result);
-}
-
-
-    catch (NoSuchMethodException e) {
-        fail("Method does not exist");
-}
-    catch(Exception e) {
-        fail("Something weird happened");
-}
-}
-@Test
-@DisplayName("")
-void bubbleSortTest1() {
-    Class<?> testClass = PracticeProblem.class;
-    try {
-        Class<?>[] cArg = {double[].class};
-        Method method = testClass.getDeclaredMethod("bubbleSort", cArg);
-  // Enter code here
-  double arr[] = {155.2, 155.1, 1, 5, 3, 2, 7, 235};
-  double arr2[] = {1, 2, 3, 5, 7, 155.1, 155.2, 235};
-    method.invoke(null, arr);
-  assertArrayEquals(arr2, arr, 0.1);
-}
-
-
-    catch (NoSuchMethodException e) {
-        fail("Method does not exist");
-}
-    catch(Exception e) {
-        fail("Something weird happened");
-}
-}
 }
